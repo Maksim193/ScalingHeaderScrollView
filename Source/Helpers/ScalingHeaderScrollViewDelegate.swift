3f7271b6e6216cca355c2be4586c3742bb0e13ac
Максим Косников <Mayden1127@yandex.ru>
2024-09-06 15:34:55 +0300

    Update introspec to ios 18

--- Source/Helpers/ScalingHeaderScrollViewDelegate.swift
+++ Source/Helpers/ScalingHeaderScrollViewDelegate.swift
@@ -9,41 +9,49 @@
 import UIKit
 
 final class ScalingHeaderScrollViewDelegate: NSObject, ObservableObject, UIScrollViewDelegate {
-    
-    var didPullToRefresh: () -> Void = { }
-    var didPullToLoadMore: () -> Void = { }
-    var didScroll: () -> Void = {}
-    var didEndDragging = {}
-    var didEndDecelerating = {}
-    var willEndDragging: (Double) -> Void = {_ in }
+	
+	var didPullToRefresh: () -> Void = { }
+	var didPullToLoadMore: () -> Void = { }
+	var didScroll: () -> Void = {}
+	var didEndDragging = {}
+	var didEndDecelerating = {}
+	var willEndDragging: (Double) -> Void = {_ in }
+	var didReachBottom: () -> Void = {}
+	
+	// weak to prevent memory leaks on iOS > 17.0 with UIScrollView in ScalingHeaderScrollView
+	weak var uiScrollView: UIScrollView?
 
-    // MARK: - UIScrollViewDelegate
+	// MARK: - UIScrollViewDelegate
 
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        didScroll()
-    }
-    
-    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        willEndDragging(velocity.y)
-    }
-    
-    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        if scrollView.contentOffset.y < -60 {
-            didPullToRefresh()
-        }
-        let maxYOffset = scrollView.contentSize.height - scrollView.bounds.height
-        if scrollView.contentOffset.y > maxYOffset + 60 {
-            didPullToLoadMore()
-        }
+	func scrollViewDidScroll(_ scrollView: UIScrollView) {
+		didScroll()
+		let maxYOffset = scrollView.contentSize.height - scrollView.bounds.height
+		if scrollView.contentOffset.y >= maxYOffset {
+			didReachBottom()
+		}
+	}
+	
+	func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+		willEndDragging(velocity.y)
+	}
+	
+	func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+		if scrollView.contentOffset.y < -60 {
+			didPullToRefresh()
+		}
+		let maxYOffset = scrollView.contentSize.height - scrollView.bounds.height
+		if scrollView.contentOffset.y > maxYOffset + 60 {
+			didPullToLoadMore()
+		}
 
-        didEndDragging()
-    }
+		didEndDragging()
+	}
 
-    func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
-        didEndDragging()
-    }
+	func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
+		didEndDragging()
+	}
 
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        didEndDecelerating()
-    }
+	func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+		didEndDecelerating()
+	}
 }
